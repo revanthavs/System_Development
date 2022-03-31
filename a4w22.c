d81670b3d045315bdb34f9ed790d3fa19b5464f9
revanthavs <atmakurirevanth@gmail.com>
2022-03-31 13:25:26 -0600

    Storing resource & job lines

--- a4w22.c
+++ a4w22.c
@@ -3,14 +3,17 @@
 
 #define MAXLINE 500
 #define MAXLINES 100
+#define MAXJOBS 30
 
-char* inputFile;
 int monitorTime;
 int NITER;
 int debug = 1; // To see debug messages debug value = 1 else debug value = 0
 char inputlines[MAXLINES][MAXLINE];
 int linesize[MAXLINES];
 int num_lines = 0;
+char resource_line[MAXLINE];
+char job_lines[MAXJOBS][MAXLINE];
+int num_jobs = 0;
 
 int main(int argv, char* argc[]){
 
@@ -20,6 +23,11 @@ int main(int argv, char* argc[]){
 	}
 
 	if (debug)	printf("Command line arguments:\n%s %s %s %s\n", argc[0], argc[1], argc[2], argc[3]);
+
+	sscanf(argc[2], "%d", &monitorTime);
+	if (debug) printf("MonitorTime: %d\n", monitorTime);
+	sscanf(argc[3], "%d", &NITER);
+	if (debug) printf("NITER: %d\n", NITER);
 
 	// Opening inputfile for read
 	FILE* file = fopen(argc[1], "r");
@@ -32,6 +40,8 @@ int main(int argv, char* argc[]){
 	while(fgets(line, MAXLINE, file)){
 		if (strlen(line) > 1){
 			if (line[0] != '#'){
+				if (line[0] == 'j' && line[1] == 'o' && line[2] == 'b')	strcpy(job_lines[num_jobs++], line);
+				if (line[0] == 'r' && line[1] == 'e' && line[2] == 's') strcpy(resource_line, line);
 				strcpy(inputlines[num_lines], line);
 				linesize[num_lines] = (int) strlen(inputlines[num_lines]);
 				num_lines++;
@@ -39,6 +49,13 @@ int main(int argv, char* argc[]){
 			}
 		}
 		memset(line, 0, MAXLINE);
+	}
+
+	if (debug){
+		printf("Jobs:\n");
+		for (int i = 0; i < num_jobs; i++) printf("%s", job_lines[i]);
+		printf("Resources:\n");
+		printf("%s", resource_line);
 	}
 
 	fclose(file);
